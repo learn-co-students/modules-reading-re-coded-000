f5fa754ca2c80f81440d28ce0eb9bf02bafbf273
Saber  Rasheed <edrees.rasheed@yahoo.com>
2017-01-29 19:00:58 +0000

    Done.

--- spec/ballerina_spec.rb
+++ spec/ballerina_spec.rb
@@ -1,15 +1,15 @@
-describe 'Ballerina' do
+describe 'Ballerina' do 
   let(:ballerina) {Ballerina.new('Anna')}
 
   it 'includes the Dance module' do
-    expect(ballerina).to have_instance_dance_methods
+    expect(ballerina).to be_a_kind_of(Dance)
   end
 
   it 'has a name' do
-    expect(ballerina.name).to eq('Anna')
+    expect(ballerina.name).to eq('Anna')  
   end
 
-  it 'extends the MetaDancing module' do
-    expect(Ballerina).to have_class_dance_methods
+  it 'extends the MetaDancing module' do 
+    expect(Ballerina).to be_a_kind_of(MetaDancing)
   end
 end
